c8c1a05b6f5502c074d1080e4d20a6b353a65405
Michael Norelli <mnorelli360@gmail.com>
2016-07-08 10:57:21 -0700

    adds some solution code, merges README update

--- starter-code/app.js
+++ starter-code/app.js
@@ -2,4 +2,22 @@
 'use strict';
 
 angular
-  .module("ToEatly", []);
+  .module("ToEatly", ['firebase'])
+  .controller('FoodCtrl', foodCtrl);
+
+foodCtrl.$inject = ['$scope','$firebaseArray'];
+
+function foodCtrl($scope, $firebaseArray) {
+  // change to your application URL, reference to the endpoint
+  // the specific endpoint foods, not the root
+  var ref = firebase.database().ref().child("foods");
+  // create a synchronized array to store a collection
+  $scope.foods = $firebaseArray(ref);
+  
+  $scope.addFood = function() {
+    $scope.foods.$add({
+      name: $scope.food.name,
+      yumminess: $scope.food.yumminess
+    });
+  };
+}
